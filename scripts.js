5fa90ff1193029994e4918a40263620c9cb2781d
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-13 21:54:27 +0000

    Final version done

--- scripts.js
+++ scripts.js
@@ -4,18 +4,25 @@ const numberTag = document.getElementById('number');
 
 const list = document.getElementById('boom-list');
 
+let clickNumber = 0;
+
+const isEmpty = (id) => {
+  return document.getElementById(id).innerHTML.trim() == ''
+}
+
 
 const onIncrementClick = () => {
   number = number * 5;
   numberTag.textContent = number;
-  if (number > (5 / (5 ** 35)) && number <= 1)
-    while (list.firstChild) {
-      list.removeChild(list.firstChild)
-    }
-  if (number === (5 ** 35)) {
+  ++clickNumber;
+  if ((clickNumber === 35 || clickNumber === -35) && isEmpty('boom-list') === true) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
+  } else if (clickNumber < 35 && clickNumber > -35) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
   }
 }
 
@@ -23,27 +30,14 @@ const onIncrementClick = () => {
 const onDecrementClick = () => {
   number = number / 5;
   numberTag.textContent = number;
-  if (number < (5 ** 35) && number >= 2)
-    while (list.firstChild) {
-      list.removeChild(list.firstChild)
-    }
-  if (number <= (5 / (5 ** 35))) {
+  --clickNumber;
+  if ((clickNumber === 35 || clickNumber === -35) && isEmpty('boom-list') === true) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
+  } else if (clickNumber < 35 && clickNumber > -35) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild)
+    }
   }
 }
-
-// const onDecrementClick = () => {
-//   number = number / 5;
-//   numberTag.textContent = number;
-//   if (number <= (5 / (5 ** 35))) {
-//     boomTag.textContent = '!bOoMm!'
-//   } else {
-//     for (let i = 0; i < list.length; i++) {
-//       if (list[i].value === 'Boom') {
-//         data.splice(i--, 1)
-//       }
-//     }
-//   }
-// }
